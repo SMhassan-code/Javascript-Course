bcafb1e6fb3ef94eada5886d1c5f92cfc504e4dd
SM Hassan <hasan151008@gmail.com>
2025-07-17 03:54:46 -0700

    learning about variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,12 @@
 const accountId = 133556
 let accountEmail = "hasan@gmail.com"
 var accountPassword = 12786
+
+/*
+Prefer not to use var
+bacause of block scope functional scope problem
+*/
+
 let accountCity;
 
 // accountId = 23 // Its value cannot be changed because it is constant
