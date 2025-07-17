152ef7f4d5be2d37da6b53960a1cb4eee2e3ebb6
SM Hassan <hasan151008@gmail.com>
2025-07-17 03:50:57 -0700

    learning about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 133556
+let accountEmail = "hasan@gmail.com"
+var accountPassword = 12786
+let accountCity;
+
+// accountId = 23 // Its value cannot be changed because it is constant
+
+accountEmail = "dua@gmail.com"
+accountPassword = 34
+accountCity = "Bahawalpur"
+
+console.log(accountId)
+console.table([accountEmail, accountPassword, accountCity])
