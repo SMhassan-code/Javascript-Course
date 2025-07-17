498a1a348374a68eede3718e90834fb7f0efbaf1
SM Hassan <hasan151008@gmail.com>
2025-07-17 12:20:18 -0700

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "Hassan"
+
+// console.log(typeof (score))
+// console.log(score)
+
+let valueInNumber = Number(score)
+
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)
+
+let isLoggedIn = "hassan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+// console.log(typeof (booleanIsLoggedIn))
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let value = 25
+
+let stringValue = String(value)
+
+console.log(stringValue)
+console.log(typeof stringValue)
