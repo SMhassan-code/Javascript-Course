96fd26ba40dd8134ef9ba2b097ed20e749138b06
SM Hassan <hasan151008@gmail.com>
2025-07-17 04:26:16 -0700

    datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert("Hello World") // we are using node JS, not browser
+
+console.log("Hassan")
+
+let name = "Hassan"
+let age = 16
+let isLoggedIn = true
+
+// There are two types of datatypes: Primitive and Object
+
+// Primitive:
+
+// number => 2
+// string => ""
+// bigInt => large value
+// boolean => true or false
+// null => standalone value
+// undefined => value is not defined
+// symbol => unique
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
