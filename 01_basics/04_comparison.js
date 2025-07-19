0ff09efacebb55f6381d02f8c9f65014d6171563
SM Hassan <hasan151008@gmail.com>
2025-07-19 05:01:09 -0700

    comparison in JS

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1)
+// console.log("2" < 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) // avoid this type of comparison
+
+console.log("2" == 2) // == changes "2" to 2 and then compares
+console.log("2" === 2) // === also check its datatype and then compares
