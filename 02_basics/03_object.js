afb448f56c5a1d68ea6a39cc74dd097ccd96e9fb
Syed Muhammad Hassan <hasan151008@gmail.com>
2025-12-25 01:38:17 +0500

    learning objects

---
+++ 02_basics/03_object.js
@@ -0,0 +1,22 @@
+// There are two ways to create an object:
+// 1. literal method (which we will use most of the time)
+// 2. constructor method (which returns a singleton object)
+// Object.create() --- example of contructor method
+
+let newSymbol = Symbol("key1")
+
+const info = {
+    user: "Hassan",
+    "full name": "Syed Muhammad Hassan",
+    [newSymbol]: 456, // symbols are declared in square brackets in objects (very IMP)
+    age: 22,
+    location: "Bahawalpur",
+    email: "Hassan@google.com",
+    isLoogedIn: false,
+    lastLoggedIn: ["Monday", "Wednesday"]
+}
+
+console.log(info.user) // common way to access values in objects but does not work everytime like in `"full name" key`
+console.log(info["user"]) // it is a better way to access anything inside the object
+console.log(info["full name"])
+console.log(info[newSymbol])
