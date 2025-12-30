06e8431e65bb8c57e8c9ca07771a8ec9da82eda0
Syed Muhammad Hassan <hasan151008@gmail.com>
2025-12-30 17:14:31 +0500

    scopes in depth

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -11,3 +11,47 @@ if (x, y, z < 100) {
 // console.log(x)
 // console.log(y)
 // console.log(z)
+
+function one () {
+    let username = "Hassan"
+
+    function two () {
+        let website = "google"
+        console.log(username)
+    }
+
+    two()
+
+    // console.log(website)
+}
+
+// one()
+
+if (!false) {
+    const username = "Ali"
+
+    if (username === "Ali") {
+        const website = " youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+// ++++++++++++++++++ interesting +++++++++++++++++++
+
+
+console.log(addTwo(20))
+
+function addTwo (num) {
+    return num + 2
+}
+
+// console.log(multiplyTen(2)) it is showing an error because the function is held by a variable. is is declared in a different way
+
+const multiplyTen = function (num) {
+    return num * 10
+}
+
+multiplyTen(2)
