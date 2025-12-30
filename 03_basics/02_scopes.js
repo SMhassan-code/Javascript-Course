cc66240230decac603e5c005d56e2c7c29230861
Syed Muhammad Hassan <hasan151008@gmail.com>
2025-12-30 15:22:09 +0500

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+let x = 20
+const y = 40
+var z = 60
+
+if (x, y, z < 100) {
+    let x = 100
+    const y = 200
+    // var z = 300 // z = 300 results in  the same problem
+}
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
