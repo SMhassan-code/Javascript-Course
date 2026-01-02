d0e94ef26fa2979b07de72969bc1db901dec9a46
Syed Muhammad Hassan <hasan151008@gmail.com>
2026-01-02 16:39:50 +0500

    while loops

---
+++ 05_iterations/while.js
@@ -0,0 +1,21 @@
+let i = 1
+// while (i <= 10) {
+//     console.log(`Value of i is ${i}`)
+//     i++
+// }
+
+const newArr = ["pluto", "jupiter", "mars"]
+
+// let count = 0
+// while (count < newArr.length) {
+//     console.log(`Value is ${newArr[count]}`)
+//     count = count + 1
+// }
+
+let score = 12
+do {
+    console.log(`Value of score is ${score}`)
+    score++
+} while (score <= 10);
+
+console.log(score)
